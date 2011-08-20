18393f91c0e70b2fee8543ccaa932fee27bce4f1
Roman Drapeko <roman.drapeko@gmail.com>
2011-08-20 10:09:32 +0100

    node-fb-signed-reuqest

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+    'name': 'node-fb-signed-request',
+    'description': 'parser for facebook signed-request',
+    'version' : '0.0.1',
+    'author': 'Roman Drapeko <roman.drapeko@gmail.com>',
+    'dependencies': {},
+    'keywords': ['facebook', 'signed request'],
+    'engines': {'node': '>= 0.4.0'},
+    'repository': 'git://github.com/drapeko/node-fb-signed-request.git'
+    'main' : 'index'   
+}
